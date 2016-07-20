d35c72358065c82f0b7a4d57660b86d784d714a9
Walter Ngo <ngo.walter@gmail.com>
2016-07-20 17:15:55 +1000

    Added tests for OptionsController, updated config.properties, implemented loading from properties file

--- src/controller/OptionsController.kt
+++ src/controller/OptionsController.kt
@@ -1,8 +1,24 @@
 package controller
 
+import com.natpryce.konfig.*
 import controller.interfaces.ViewChanger
 import tornadofx.Controller
 
+// TODO: implement saving to properties file
 class OptionsController : ViewChanger, Controller() {
-    // TODO: implement reading/saving from properties file
+
+    // Property keys
+    object business : PropertyGroup() {
+        val name by stringType
+        val abn by stringType
+    }
+
+    // Load config.properties file from resources
+    val option = ConfigurationProperties.fromResource("config.properties")
+
+    // Get business name from configuration
+    fun getBusinessName() = option[business.name]
+
+    // Get business number from configuration
+    fun getAbn() = option[business.abn]
 }
--- test/OptionsControllerTest.kt
+++ test/OptionsControllerTest.kt
@@ -1,8 +1,19 @@
-import io.kotlintest.specs.FunSpec
+import io.kotlintest.specs.FlatSpec
+import controller.OptionsController
 
-class OptionsControllerTest : FunSpec() {
+class OptionsControllerTest : FlatSpec() {
+
+    val optionController = OptionsController()
 
     init {
+
+        "getBusinessName" should "return 'Example Business'" {
+            optionController.getBusinessName() shouldBe "Example Business"
+        }
+
+        "getAbn" should "return '51 824 753 556'" {
+            optionController.getAbn() shouldBe "51 824 753 556"
+        }
 
     }
 }
