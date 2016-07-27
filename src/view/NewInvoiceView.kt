8c97843451c6d5ca8069915bf764ca04147627b6
Walter Ngo <ngo.walter@gmail.com>
2016-07-27 15:39:11 +1000

    Changed BoardPane to GridPane

--- src/view/NewInvoiceView.kt
+++ src/view/NewInvoiceView.kt
@@ -2,22 +2,21 @@ package view
 
 import controller.InvoiceController
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.GridPane
 import tornadofx.View
 import tornadofx.button
 import tornadofx.center
 import tornadofx.label
 
 class NewInvoiceView : View() {
-    override val root = BorderPane()
+    override val root = GridPane()
     val mainMenu : MainMenu by inject()
     val invoiceController : InvoiceController by inject()
 
     init {
         with(root) {
-            center {
-                button("Back") {
-                    setOnAction {invoiceController.changeView(mainMenu) }
-                }
+            button("Back") {
+                setOnAction {invoiceController.changeView(mainMenu) }
             }
         }
     }
